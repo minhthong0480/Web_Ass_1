7bc2e912912d38ec629597b5b815f9bd76ad6a7e
Minh Thong <73870657+minhthong0480@users.noreply.github.com>
2021-05-07 11:15:40 +0700

    Update auto_scroll.js

--- JavaScript/auto_scroll.js
+++ JavaScript/auto_scroll.js
@@ -1,14 +1,17 @@
-var slideIndex = 0;
-showSlides();
+var firstval = 0;
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("scroll_product");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
+function Carousel() {
+    firstval += 2;
+    parent = document.querySelector(".inside-content");
+    parent.style.left = "-" + firstval + "px";
+    if (!(firstval % 150)) {
+        setTimeout(Carousel, 3000);
+        firstval = 0;
+        var firstChild = parent.firstElementChild;
+        parent.appendChild(firstChild);
+        parent.style.left= 0;
+        return;
+    }
+    runCarousel = setTimeout(Carousel, 10);
 }
+Carousel();
