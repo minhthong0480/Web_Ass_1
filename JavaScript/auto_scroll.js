8ddd96a3addcf4bebb8fcf50ca43c30acd509ab7
Minh Thong <s3852882@rmit.edu.vn>
2021-05-07 14:46:22 +0700

    add auto scroll js

--- JavaScript/auto_scroll.js
+++ JavaScript/auto_scroll.js
@@ -1,19 +1,22 @@
-var firstval = 0;
-var runSlider;
+var firstinterval = 0;
 
-function Carousel() {
-    clearTimeout(runSlider);
-        firstval += 2;
-        parent = document.querySelector('.inside-content');
-        parent.style.left = "-" + firstval + "px";
-        if (!(firstval % 130)) {
-            setTimeout(Carousel, 3000);
-            firstval = 0;
+
+function auto_scroll() {
+    
+        firstinterval += 2;
+        parent = document.querySelector('.inside-content-1');
+        parent.style.left = "-" + firstinterval + "px";
+        if (!(firstinterval % 130)) {
+            setTimeout(auto_scroll, 1000);
+            firstinterval = 0;
             var firstChild = parent.firstElementChild;
             parent.appendChild(firstChild);
             parent.style.left= 0;
             return;
         }
-        runCarousel = setTimeout(Carousel, 10);
+        runauto_scroll = setTimeout(auto_scroll, 10);
     }
-    Carousel();
+    auto_scroll();
+
+    mouse_over = document.getElementsByClassName("inside-content-1");
+    
