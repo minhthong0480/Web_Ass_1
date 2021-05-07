3f65e54931bd1abe9e78a5e315a9de6f838fccc4
Minh Thong <s3852882@rmit.edu.vn>
2021-05-07 11:53:14 +0700

    fix css browse store/product

--- JavaScript/auto_scroll.js
+++ JavaScript/auto_scroll.js
@@ -1,17 +1,19 @@
 var firstval = 0;
+var runSlider;
 
 function Carousel() {
-    firstval += 2;
-    parent = document.querySelector(".inside-content");
-    parent.style.left = "-" + firstval + "px";
-    if (!(firstval % 150)) {
-        setTimeout(Carousel, 3000);
-        firstval = 0;
-        var firstChild = parent.firstElementChild;
-        parent.appendChild(firstChild);
-        parent.style.left= 0;
-        return;
+    clearTimeout(runSlider);
+        firstval += 2;
+        parent = document.querySelector('.inside-content');
+        parent.style.left = "-" + firstval + "px";
+        if (!(firstval % 130)) {
+            setTimeout(Carousel, 3000);
+            firstval = 0;
+            var firstChild = parent.firstElementChild;
+            parent.appendChild(firstChild);
+            parent.style.left= 0;
+            return;
+        }
+        runCarousel = setTimeout(Carousel, 10);
     }
-    runCarousel = setTimeout(Carousel, 10);
-}
-Carousel();
+    Carousel();
