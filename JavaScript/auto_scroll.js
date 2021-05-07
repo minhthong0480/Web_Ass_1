0168bdcd335dd98d41f0047a74d049367db45f66
Minh Thong <s3852882@rmit.edu.vn>
2021-05-07 15:33:01 +0700

    add some adjuctment for auto scroll

--- JavaScript/auto_scroll.js
+++ JavaScript/auto_scroll.js
@@ -18,5 +18,22 @@ function auto_scroll() {
     }
     auto_scroll();
 
+function auto_scroll_1() {
+    
+        firstinterval += 2;
+        parent = document.querySelector('.inside-content-2');
+        parent.style.left = "-" + firstinterval + "px";
+        if (!(firstinterval % 130)) {
+            setTimeout(auto_scroll_1, 1000);
+            firstinterval = 0;
+            var firstChild = parent.firstElementChild;
+            parent.appendChild(firstChild);
+            parent.style.left= 0;
+            return;
+        }
+        runauto_scroll = setTimeout(auto_scroll_1, 10);
+    }
+    auto_scroll_1();
+
     mouse_over = document.getElementsByClassName("inside-content-1");
     
